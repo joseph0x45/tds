998da2ec0d574f60cb91ad0f043848c46b73e25d
joseph0x45 <zozozozeph@gmail.com>
2024-09-21 17:22:45 +0000

    setting up database schema

---
+++ server/schema.sql
@@ -0,0 +1,29 @@
+create table devices (
+  id text not null primary key,
+  version text not null,
+  model text not null,
+  number text not null, -- the phone number inside the device
+  data_on_number integer not null default 0
+);
+
+create table users (
+  id text not null primary key,
+  phone text not null default '',
+  email text not null default '',
+  password text not null
+);
+
+create table subscriptions (
+  id text not null primary key,
+  user_id text not null references users(id),
+  bought_on date not null,
+  expires_on date not null
+);
+
+create table device_positions (
+  device_id text not null references devices(id),
+  latitude decimal(9,6) not null,
+  longitude decimal(9,6) not null,
+  logged_at timestamp not null default now(),
+  primary key (device_id, logged_at)
+);
